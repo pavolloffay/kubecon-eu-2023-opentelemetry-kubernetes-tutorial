b9e2affcb866c850d387fee0541cb72d4ea37e09
svrnm <neumanns@cisco.com>
2023-03-21 18:07:00 +0100

    Minor UI fix
    Signed-off-by: svrnm <neumanns@cisco.com>

--- app/frontend/index.js
+++ app/frontend/index.js
@@ -12,6 +12,7 @@ app.get('/', (req, res) => {
         data.push(chunk);
       });
       response.on('end', () => {
+        res.write('Player 1 rolls: ' + player1 + '\n')
         resolve(JSON.parse(Buffer.concat(data).toString()));
       })
     })
@@ -25,14 +26,15 @@ app.get('/', (req, res) => {
         data.push(chunk);
       });
       response.on('end', () => {
+        res.write('Player 2 rolls: ' + player2 + '\n')
         resolve(JSON.parse(Buffer.concat(data).toString()));
       })
     })
   })
 
   Promise.all([p1, p2]).then(([player1, player2]) => {
-    res.write('Player 1 rolls: ' + player1 + '\n')
-    res.write('Player 2 rolls: ' + player2 + '\n')
+
+    
     if(player1 > player2) {
       res.end('Player 1 wins')
     } else if(player2 > player1) {
