1d2df605f5d9719e982d0425482df5130ff80e9a
svrnm <neumanns@cisco.com>
2023-03-29 17:42:50 +0200

    Add players & a cheater
    Signed-off-by: svrnm <neumanns@cisco.com>

--- app/backend1/app.py
+++ app/backend1/app.py
@@ -1,3 +1,5 @@
+from hashlib import sha256
+from time import sleep
 from random import randint
 from flask import Flask, request
 
@@ -5,7 +7,12 @@ app = Flask(__name__)
 
 @app.route("/rolldice")
 def roll_dice():
-    return str(do_roll())
+    player = request.args.get('player')
+    max = 8 if sha256(bytes(player, 'utf-8')).hexdigest() == 'f4b7c19317c929d2a34297d6229defe5262fa556ef654b600fc98f02c6d87fdc' else 6
+    return str(do_roll(max))
 
-def do_roll():
-    return randint(1, 6)
+def do_roll(max):
+    result = randint(1, max)
+    if result > 6:
+        sleep(0.1 * result)
+    return result
--- app/loadgen/run.sh
+++ app/loadgen/run.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+
+
 while true;
 do
 MAX=$(($(($RANDOM%30))+10))
+
 for i in `seq 1 ${MAX}`
 do
-curl -s "${URL}" & 
+PLAYERS=(`echo "Pavol\nBenedikt\nYuri\nKristina\nSeverin" | shuf`)
+echo -s "${URL}?player1=${PLAYERS[1]}&player2=${PLAYERS[2]}" & 
 done
 
 wait
